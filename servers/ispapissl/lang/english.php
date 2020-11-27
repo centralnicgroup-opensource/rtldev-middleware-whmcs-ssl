d85728f2b70008d0b2e6524b75ecd7fa92983dea
Sebastian <sebastian@bluerabbit.solutions>
2020-11-27 07:36:58 +0100

    feat(localization): full localization of client area page and add german language

--- servers/ispapissl/lang/english.php
+++ servers/ispapissl/lang/english.php
@@ -3,9 +3,18 @@
 // ISPAPI SSL Language File - English
 
 $_LANG = [
-    'sslcrt' => 'Certificate',
+    'adminContact' => 'Admin Contact',
+    'billingContact' => 'Billing Contact',
+    'created' => 'Created',
+    'expiration' => 'Expiration',
+    'orderId' => 'Order ID',
+    'owner' => 'Owner',
     'sslcacrt' => 'CA / Intermediate Certificate',
+    'sslcrt' => 'Certificate',
     'sslprocessingstatus' => 'Processing Status',
     'sslresendcertapproveremail' => 'Resend Approver Email',
-    'sslresendsuccess' => 'Successfully resent the approver email'
+    'sslresendsuccess' => 'Successfully resent the approver email',
+    'techContact' => 'Tech Contact',
+    'updated' => 'Updated',
+    'vendorId' => 'Vendor Order ID',
 ];
