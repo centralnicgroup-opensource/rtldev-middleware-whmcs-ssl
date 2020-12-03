e8392d0cf00393383b56234f9b07ad456a4413b6
Sebastian <sebastian@bluerabbit.solutions>
2020-12-03 06:57:08 +0100

    feat(localization): localize addon and add italian translations

--- addons/ispapissl_addon/lang/english.php
+++ addons/ispapissl_addon/lang/english.php
@@ -1,5 +1,12 @@
 <?php
 
-$_ADDONLANG = [
+// ISPAPI SSL Addon Language File - English
 
+$_ADDONLANG = [
+    'productGroupDescription' => "Choose product group new products should be assigned to",
+    'roundAllCurrencies' => "Apply rounding to all currencies",
+    'roundAllCurrenciesDescription' => "Also round the converted prices",
+    'products' => "products",
+    'certificate' => "SSL Certificate",
+    'autoRegistration' => "Auto-registration enabled"
 ];
---
+++ addons/ispapissl_addon/lang/italian.php
@@ -0,0 +1,12 @@
+<?php
+
+// ISPAPI SSL Addon Language File - Italian
+
+$_ADDONLANG = [
+    'productGroupDescription' => "Scegli il gruppo al quale assegnare i nuovi prodotti",
+    'roundAllCurrencies' => "Arrotonda tutte le valute",
+    'roundAllCurrenciesDescription' => "Arrotonda i prezzi convertiti",
+    'products' => "prodotti",
+    'certificate' => "Certificato SSL",
+    'autoRegistration' => "Registrazione automatica attivata"
+];
